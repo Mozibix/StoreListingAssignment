6e4ac5e86c19545d99b8a3d25f467f39056b0079
Mozibix <opuzmoses@gmail.com>
2023-03-31 23:36:03 +0100

    chores: updating mapped data to the DOM and updating sort functions

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,88 +1,120 @@
-import React from "react";
-import "../styles/HotelStyle.css"
-import User from "../images/kishan.jpg"
-import data from "../data/data.json"
-import RestaurantCard from "../components/RestaurantCard"
+import React, { useState, useEffect } from "react";
+import "../styles/HotelStyle.css";
+import User from "../images/kishan.jpg";
+import data from "../data/data.json";
+import RestaurantCard from "../components/RestaurantCard";
+import Orders from "./Orders";
+
 class Hotels extends React.Component {
-    constructor(){
-        super();
-        this.state={
-            list: data
-        };
-    }
-
-    
-
-    sortMenu=(e)=>{
-        if (e.target.value === 'rating'){
-            this.setState({
-                list: data.sort(function(a,b){return b.rating - a.rating})
-            })
-            
-        }
-        else if (e.target.value === 'review'){
-            this.setState({
-                list: data.sort(function(a,b){return b.reviews - a.reviews})
-            })
-            
-        }
-        else if (e.target.value === 'name'){
-            function compareName  (a, b)  {
-                // case-insensitive comparison
-                a = a.toLowerCase();
-                b = b.toLowerCase();
-              
-                return (a < b) ? -1 : (a > b) ? 1 : 0;
-              }
-            this.setState({
-                list: data.sort(function(a,b){return compareName(a.name, b.name)})
-            })
-            
-        }
-        
-    }
-
-    render(){
-        return(
-        <div>
-            <div className="nav">
-                <div id="logo">
-                    <h2>FOODIE</h2>
-                </div>
-
-                <div id="user">
-                    <div className="name">Hello, Kishan</div>
-                    <div className="profile">
-                        <img src={User} alt="profile" id="img" height="45" width="45"/>
-                    </div>
-                </div>
-            </div>
-            
-            <div className="maincart">
-
-            <div id="menubar">
-                <h2 id="menu-title">Choose Your Favourite One</h2>
-                <p id ="sort">  Sort by &nbsp; &nbsp;
-                    <select id="sort-metrics" defaultValue={"none"} onChange={(e) => this.sortMenu(e)}>
-                        <option value="none" disabled hidden>None</option>
-                        <option class="sort-option" value="name">Name</option>
-                        <option class="sort-option" value="rating">Ratings</option>
-                        <option class="sort-option" value="review">Reviews</option>
-                    </select>
-                </p>
-            </div>
-            {this.state.list.map(
-                x => 
-                    <RestaurantCard thumbnail_image={x.thumbnail_image} name = {x.name} cuisines = {x.cuisines} rating = {x.rating} reviews = {x.reviews}/>
-                    
-            )}
-
-        
-            
-            </div>
-        </div>
-        )
+  constructor() {
+    super();
+    this.state = {
+      list: data,
     };
+  }
+
+  sortMenu = (e) => {
+    if (e.target.value === "rating") {
+      this.setState({
+        list: data.sort(function (a, b) {
+          return b.rating - a.rating;
+        }),
+      });
+    } else if (e.target.value === "price") {
+      this.setState({
+        list: data.sort(function (a, b) {
+          return b.price - a.price;
+        }),
+      });
+    } else if (e.target.value === "name") {
+      function compareName(a, b) {
+        // case-insensitive comparison
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        return a < b ? -1 : a > b ? 1 : 0;
+      }
+      this.setState({
+        list: data.sort(function (a, b) {
+          return compareName(a.name, b.name);
+        }),
+      });
+    } else if (e.target.value === "veg") {
+      this.setState({
+        list: data.filter(function (a, b) {
+          return a.isVeg == true ? a : b;
+        }),
+      });
+    } else if (e.target.value === "non-veg") {
+      this.setState({
+        list: data.filter(function (a, b) {
+          return a.isVeg == false ? a.isVeg : b;
+        }),
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="nav">
+          <div id="logo">
+            <h2>FOODIE</h2>
+          </div>
+
+          <div id="user">
+            <div className="name">Hello, Kishan</div>
+            <div className="profile">
+              <img src={User} alt="profile" id="img" height="45" width="45" />
+            </div>
+          </div>
+        </div>
+
+        <div className="maincart">
+          <div id="menubar">
+            <h2 id="menu-title">Choose Your Favourite One</h2>
+            <p id="sort">
+              {" "}
+              Sort by &nbsp; &nbsp;
+              <select
+                id="sort-metrics"
+                defaultValue={"none"}
+                onChange={(e) => this.sortMenu(e)}
+              >
+                <option value="none" disabled hidden>
+                  None
+                </option>
+                <option class="sort-option" value="name">
+                  name{" "}
+                </option>
+                <option class="sort-option" value="veg">
+                  vegetarian
+                </option>
+                <option class="sort-option" value="non-veg">
+                  non-vegetarian{" "}
+                </option>
+                <option class="sort-option" value="price">
+                  Price{" "}
+                </option>
+                <option class="sort-option" value="rating">
+                  Rating{" "}
+                </option>
+              </select>
+            </p>
+          </div>
+          {this.state.list.map((x) => (
+            <RestaurantCard
+              thumbnail_image={x.img_url}
+              name={x.name}
+              rating={x.rating}
+              price={x.price}
+              description={x.description}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Hotels;
